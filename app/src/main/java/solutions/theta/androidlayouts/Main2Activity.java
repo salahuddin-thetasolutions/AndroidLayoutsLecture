6cb32c5aabec834dbdae3cf604237c9ee427fa8e
SALAHUDIN` <salahudinhafiz@gmail.com>
2018-02-13 18:19:25 +0500

    Widgets declaration & initialization plus clicklistener in activity

--- app/src/main/java/solutions/theta/androidlayouts/Main2Activity.java
+++ app/src/main/java/solutions/theta/androidlayouts/Main2Activity.java
@@ -2,12 +2,29 @@ package solutions.theta.androidlayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText mUserName,mPassword;
+    Button mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        mUserName=(EditText) findViewById(R.id.etUserName);
+        mPassword=(EditText) findViewById(R.id.etPassword);
+        mLogin=(Button)findViewById(R.id.btnLogin);
+        mLogin.setOnClickListener(this);
+        mSignup.
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "login", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "signup", Toast.LENGTH_SHORT).show();
     }
 }
